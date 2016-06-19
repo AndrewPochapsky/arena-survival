da4ff54cd0dd22ad7faf95420b5b7f54ef1737b0
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2016-06-19 15:48:08 -0400

    Imported the menus

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+        
+    static MusicPlayer instance = null;
+
+    public AudioClip startClip;
+    public AudioClip gameClip;
+    public AudioClip endClip;
+
+    private AudioSource music;
+
+    void Start ()
+    {
+        music = GetComponent<AudioSource>();
+        Debug.Log("Music Player Awake " + GetInstanceID());
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            print("Duplicated music player destroyed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.clip = startClip;
+            music.loop = true;
+            music.Play();
+        }
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (music == null)
+        {
+            music = GetComponent<AudioSource>();
+        }
+        else {
+            music.Stop();
+        }
+        if (level == 0)
+        {
+            music.clip = startClip;
+        }
+
+        if (level == 1)
+        {
+            music.clip = gameClip;
+        }
+
+        if (level == 2)
+        {
+            music.clip = endClip;
+        }
+        music.loop = true;
+        music.Play();
+    }
+
+	// Use this for initialization
+     
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+}
