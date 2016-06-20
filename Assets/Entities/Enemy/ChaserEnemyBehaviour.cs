195cc166f4664ef64f3e13a0d1c789c04a85e36f
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2016-06-20 16:19:00 -0400

    removed unnecessary files, enemies now die properly, if projectile hits wall it gets destroyed

---
+++ Assets/Entities/Enemy/ChaserEnemyBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChaserEnemyBehaviour : MonoBehaviour
+{
+    public float health = 150f;
+    public int scoreValue = 150;
+    //private ScoreKeeper scoreKeeper;
+    public AudioClip deathSound;
+    void Start()
+    {
+        //scoreKeeper = GameObject.Find("Score").GetComponent<ScoreKeeper>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Projectile missle = col.gameObject.GetComponent<Projectile>();
+        if (missle)
+        {
+            health -= missle.GetDamage();
+            missle.Hit();
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+        //scoreKeeper.Score(scoreValue);
+        AudioSource.PlayClipAtPoint(deathSound, transform.position);
+    }
+
+}
---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shredder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Projectile missle = col.gameObject.GetComponent<Projectile>();
+        if (missle)
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
+
+
