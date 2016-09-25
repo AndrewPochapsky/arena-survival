873d2989765c025abb2565588981485e2c217d64
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2016-09-25 15:50:57 -0400

    Fixed hard crashes when changing resolution :)

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -4,80 +4,19 @@ using UnityEngine.UI;
 
 public class VideoManager : MonoBehaviour {
 
-    public Dropdown resDropDown;
+    private Dropdown resDropDown;
 
 	// Use this for initialization
 	void Start () {
+        resDropDown = GetComponent<Dropdown>();
+        resDropDown.interactable = true;
         SetResolution();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        PlayerPrefs.SetInt("height", Screen.currentResolution.height);
-        PlayerPrefs.SetInt("width", Screen.currentResolution.width);
+       
 
-        PlayerPrefs.SetInt("ddv", resDropDown.value);
-
-        if (resDropDown.value == 0)
-        {
-            Screen.SetResolution(1920, 1200, true);           
-        }
-        if (resDropDown.value == 1)
-        {
-            Screen.SetResolution(1920, 1080, true);           
-        }
-        if (resDropDown.value == 2)
-        {
-            Screen.SetResolution(1680, 1050, true);          
-        }
-        if (resDropDown.value == 3)
-        {
-            Screen.SetResolution(1600, 1200, true);           
-        }
-        if (resDropDown.value == 4)
-        {
-            Screen.SetResolution(1600, 1024, true);
-        }
-        if (resDropDown.value == 5)
-        {
-            Screen.SetResolution(1600, 900, true);
-        }
-        if (resDropDown.value == 6)
-        {
-            Screen.SetResolution(1366, 768, true);
-        }
-        if (resDropDown.value == 7)
-        {
-            Screen.SetResolution(1280, 1024, true);
-        }
-        if (resDropDown.value == 8)
-        {
-            Screen.SetResolution(1280, 960, true);
-        }
-        if (resDropDown.value == 9)
-        {
-            Screen.SetResolution(1280, 800, true);
-        }
-        if (resDropDown.value == 10)
-        {
-            Screen.SetResolution(1280, 768, true);
-        }
-        if (resDropDown.value == 11)
-        {
-            Screen.SetResolution(1280, 720, true);
-        }
-        if (resDropDown.value == 12)
-        {
-            Screen.SetResolution(1152, 864, true);
-        }
-        if (resDropDown.value == 13)
-        {
-            Screen.SetResolution(1024, 768, true);
-        }
-        if (resDropDown.value == 14)
-        {
-            Screen.SetResolution(800, 600, true);
-        }
     }
 
     public void SetResolution()
@@ -85,6 +24,54 @@ public class VideoManager : MonoBehaviour {
         Screen.SetResolution(PlayerPrefs.GetInt("width"), PlayerPrefs.GetInt("height"), true);
 
         resDropDown.value = PlayerPrefs.GetInt("ddv");
+    }
+
+    public void ChangeRes()
+    {
+        PlayerPrefs.SetInt("height", Screen.currentResolution.height);
+        PlayerPrefs.SetInt("width", Screen.currentResolution.width);
+
+        PlayerPrefs.SetInt("ddv", resDropDown.value);
+
+        if (resDropDown.value == 0)
+        {
+            Screen.SetResolution(1920, 1200, true);
+        }
+        if (resDropDown.value == 1)
+        {
+            Screen.SetResolution(1920, 1080, true);
+        }
+        if (resDropDown.value == 2)
+        {
+            Screen.SetResolution(1366, 768, true);
+        }
+        if (resDropDown.value == 3)
+        {
+            Screen.SetResolution(1280, 1024, true);
+        }
+        if (resDropDown.value == 4)
+        {
+            Screen.SetResolution(1280, 800, true);
+        }
+        if (resDropDown.value == 5)
+        {
+            Screen.SetResolution(1024, 768, true);
+        }
+    }
+
+    public void InitiateDisable()//For UI cause it cant use the IEnumerator directly
+    {
+        StartCoroutine(DisableDropDown());
+    }
+
+    public IEnumerator DisableDropDown()
+    {
+        Debug.Log("DropDown Disabled");
+        resDropDown.interactable = false;
+        yield return new WaitForSeconds(2);
+        resDropDown.interactable= true;
+        Debug.Log("DownDown Enabled");
+
     }
 
 }
